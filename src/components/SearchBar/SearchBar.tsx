2c431806345df06dde5c5a045300bd64413998eb
jeniferbenavent <jbenaventzapata@gmail.com>
2023-05-23 15:15:15 +0200

    New SearchBar and minimal changes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -20,42 +20,15 @@ function SearchBar(props: SearchBarProps) {
   
   
   return (
-    <>
-      <form onSubmit={ handleInputChange }>
-        <input type="search" value={city} onInput={handleInputValueChange} />
+    <form onSubmit={ handleInputChange } className="search__container">
+      <div className="search__input-container">
+        <input className="search__input" type="search" placeholder="Tokyo, Paris, Sevilla..." value={city} onInput={handleInputValueChange} />
         <button type="submit">
           <i className="fa fa-search"></i>
         </button>
-      </form>
-    </>
-
+      </div>
+    </form>
   );
 }
 
 export default SearchBar;
-/*
-function SearchBar(props: SearchBarProps) {
-
-  const [city, setCity] = useState(''); // Ciudad escrita por el usuario
-
-  const handleInputChange = useCallback((event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); // Previene que la página se recargue al enviar el formulario
-    props.onSearchSubmit(city);
-    setCity('');
-    event.currentTarget.reset(); // Limpia el valor del input después de enviar el formulario
-  }, [city, props.onSearchSubmit]);
-
-  const handleInputValueChange = useCallback((event: FormEvent<HTMLInputElement>) => {
-    setCity(event.currentTarget.value);
-  }, []);
-  
-    <div className="div-main-search">
-      <form onSubmit={ handleInputChange }>
-          <button type="submit" className="search-btn">
-            <img src="src/assets/icons/placeholder.png" width="25px"/>
-          </button>
-        <input type="text" className="search-div" placeholder="Valencia, Seoul, Tokyo..." value={city} onInput={handleInputValueChange} />
-      </form>
-  </div>*/
-
-
--- src/components/WeatherDataCard/WeatherDataCard.tsx
+++ src/components/WeatherDataCard/WeatherDataCard.tsx
@@ -10,27 +10,25 @@ function WeatherData(props: WeatherDataProps) {
   }
   
   return (
-    <>
-      <div className="weather-card">
-        <div className="weather-card-dayTime">
-        <h3 className="weather-card-dayTime-text">{ moment().format('dddd').slice(0, 3) } { moment().format('Do') }</h3>
-          <p>Hoy en {props.weatherData.name}</p>
-        </div>
-
-        <div className="weather-card-icon">
-          <WeatherIcon weatherId={props.weatherData.weather[0].id} big title={props.weatherData.weather[0].description}/>
-        </div>
-
-        <div className="weather-card-degrees">
-          <h2 className="weather-card-degrees-text">
-            {Math.trunc(props.weatherData.main.temp)}ºC
-          </h2>
-          <p className="weather-card-degrees-feelsLike">
-            Feels like {Math.trunc(props.weatherData.main.feels_like)}ºC
-          </p>
-        </div>
+    <div className="weather-card">
+      <div className="weather-card-dayTime">
+      <h3 className="weather-card-dayTime-text">{ moment().format('dddd').slice(0, 3) } { moment().format('Do') }</h3>
+        <p>Today in {props.weatherData.name}</p>
       </div>
-    </>
+
+      <div className="weather-card-icon">
+        <WeatherIcon weatherId={props.weatherData.weather[0].id} big title={props.weatherData.weather[0].description}/>
+      </div>
+
+      <div className="weather-card-degrees">
+        <h2 className="weather-card-degrees-text">
+          {Math.trunc(props.weatherData.main.temp)}ºC
+        </h2>
+        <p className="weather-card-degrees-feelsLike">
+          Feels like {Math.trunc(props.weatherData.main.feels_like)}ºC
+        </p>
+      </div>
+    </div>
   );
 }
 
