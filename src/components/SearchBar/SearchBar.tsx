3ecf06174a8080af90e3d17e3fa106dc651eaf5b
jeniferbenavent <jbenaventzapata@gmail.com>
2023-05-24 13:22:32 +0200

    New search icon and other styles changes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -24,7 +24,7 @@ function SearchBar(props: SearchBarProps) {
       <div className="search__input-container">
         <input className="search__input" type="search" placeholder="Tokyo, Paris, Sevilla..." value={city} onInput={handleInputValueChange} />
         <button type="submit">
-          <i className="fa fa-search"></i>
+        <img src="src/assets/images/busqueda.png" alt="Search icon"/>
         </button>
       </div>
     </form>
